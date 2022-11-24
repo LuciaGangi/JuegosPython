509ad11522a3301899a3c2f951fedaf8673d526d
unknown <lucia.gangi@mi.unc.edu.ar>
2022-11-24 19:37:49 -0300

    initial commit

---
+++ adivina_el_numero.py
@@ -0,0 +1,10 @@
+import random
+
+def adivina_el_número(x):
+    
+    print("==========================")
+    print(" ¡Bienvenido(a) al juego! ")
+    print("==========================")
+    print("Tu meta es adivinar el número generado por la computadora.")
+    
+    número_aleatorio = random.randint()
---
+++ historias_locas.py
@@ -0,0 +1,40 @@
+# Concatenar cadenas de caracteres.
+# Supongamos que queremos crear una cadena que diga:
+# Aprende a programar con ___________.
+
+#organización = "freeCodeCamp"
+
+#print("Aprende a programar con " + organización)
+#print("Aprende a programar con {}".format(organización))
+#print(f"Aprende a programar con {organización}") #f-string
+
+# Mad Libs (Historias Locas)
+
+sustantivo_singular_m1 = input("Sustantivo singular (masculino): ")
+sustantivo_singular_f1 = input("Sustantivo singular (femeninino): ")
+verbo_gerundio = input("Verbo (gerundio)")
+sustantivo_plural = input("Sustantivo (plural): ") 
+sustantivo_singular_f2 = input("Sustantivo singular (femeninino): ")
+sustantivo_plural_m1 = input("Sustantivo plural (masculino): ")
+sustantivo_singular_m2 = input("Sustantivo singular (masculino): ")
+verbo_pretimper_3_persona1 = input("Verbo pret. imperfecto (3ra persona)")
+adj_plural_m3 = input("Adjetivo plural (masculino)")
+sustantivo_plural_m2 = input("Sustantivo plural (masculino): ")
+adj_f2 = input("Adjetivo (femeninino)")
+sustantivo_singular_f6 = input("Sustantivo singular (femeninino): ")
+verbo_inf = input("Verbo (infinitivo)")
+verbo_futuimper_2per_plural = input("Verbo futuro imperfecto plural (2da persona)")
+sustantivo_singular_f8 = input("Sustantivo singular (femeninino): ")
+sustantivo_plural_m7 = input("Sustantivo plural (masculino): ")
+adj_plural_m2 = input("Adjetivo plural (masculino)")
+sustantivo_plural_m8 = input("Sustantivo plural (masculino): ")
+sustantivo_plural_m9 = input("Sustantivo plural (masculino): ")
+sustantivo_singular_m6 = input("Sustantivo singular (masculino): ")
+adj_plural_m1 = input("Adjetivo plural (masculino)")
+sustantivo_plural_m5 = input("Sustantivo plural (masculino): ")
+
+
+madlib = f"Vivían en un {sustantivo_singular_m1}. Después, en una {sustantivo_singular_f1} que viajaba en el mar. Dieron vueltas y vueltas por el mundo. Y ahora, {verbo_gerundio} {sustantivo_plural}, viven en la {sustantivo_singular_f2}.Los {sustantivo_plural_m1} del {sustantivo_singular_m2} {verbo_pretimper_3_persona1} {adj_plural_m3}. También los {sustantivo_plural_m2} de la {sustantivo_singular_f1} viajera dormían en sus {sustantivo_plural}. Pero un día se fueron de la {sustantivo_singular_f1} y del {sustantivo_singular_m2}, y, sin querer, llegaron a la {adj_f2} {sustantivo_singular_f2}. Una {sustantivo_singular_f6} les dijo cuando llegaron: Nunca podrán {verbo_inf} a la {sustantivo_singular_f1}, ni al {sustantivo_singular_m2} {verbo_futuimper_2per_plural}.¡Y en una {sustantivo_singular_f8} los encerraron! ¡Sin {sustantivo_plural_m1}, qué {adj_plural_m1} se quedaron los {sustantivo_plural_m5}! Nunca más verían los {sustantivo_plural_m1}, los {sustantivo_plural_m7}, los {adj_plural_m2} {sustantivo_plural_m8} del mundo, NUNCA MÁS. ¡Y POR ESO INVENTARON LOS {sustantivo_plural_m9}! ¡Y EL PRIMERO DE TODOS FUE UN {sustantivo_singular_m6}!"
+
+
+print(madlib)
