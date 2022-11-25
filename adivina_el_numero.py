b2a8428569172f55d1598ef1d428dc5fa354d1ca
unknown <lucia.gangi@mi.unc.edu.ar>
2022-11-25 17:31:32 -0300

    first commit

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,5 +1,6 @@
 import random
 
+
 def adivina_el_número(x):
     
     print("==========================")
@@ -7,4 +8,20 @@ def adivina_el_número(x):
     print("==========================")
     print("Tu meta es adivinar el número generado por la computadora.")
     
-    número_aleatorio = random.randint()
+    número_aleatorio = random.randint(1, x) # Número aleatorio entre 1 y X
+    
+    predicción = 0
+    
+    while predicción != número_aleatorio:
+        #El usuario ingresa un número
+        predicción = int(input(f"Adivina un número entre 1 y {x}: ")) # f-string
+        
+        if predicción < número_aleatorio:
+            print("Intenta otra vez. Este número es muy pequeño.")
+        elif predicción > número_aleatorio:
+            print("Intenta otra vez. Este número es muy grande.")
+        
+    print(f"¡Felicitaciones! Adivinaste el número {número_aleatorio} correctamente.")
+    
+    
+adivina_el_número(10)        
